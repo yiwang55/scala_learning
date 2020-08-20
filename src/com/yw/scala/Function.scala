b85f20f92831c8e563fc43b870ace1ef8c87d7dc
wangyi <wangyi@ghcchina.com.cn>
2020-08-20 15:40:57 +0800

    scala learning p5 finish

--- src/com/yw/scala/Function.scala
+++ src/com/yw/scala/Function.scala
@@ -1,5 +1,7 @@
 package com.yw.scala
 
+import java.util.Date
+
 object Function {
   def main(args: Array[String]): Unit = {
 //    def max(a :Int, b:Int) = {
@@ -52,8 +54,76 @@ object Function {
     /**
      * 匿名函数
      */
-    def fun = (a:Int, b:Int)=>{
-      a+b
+//    def fun = (a:Int, b:Int)=>{
+//      a+b
+//    }
+//    println(fun(100,200))
+
+    /**
+     * 偏应用函数
+     */
+//    def showLog(date:Date, log:String) ={
+//      println(s"date is $date , log is $log");
+//    }
+//
+//    val date  = new Date();
+////    showLog(date, "a")
+////    showLog(date, "b")
+////    showLog(date, "c")
+//
+//    def fun = showLog(date, _:String)
+//    fun("aaa")
+//    fun("bbb")
+//    fun("ccc")
+
+    /**
+     * 高阶函数
+     */
+    //方法的参数是函数
+//    def fun(a:Int,b:Int) : Int = {
+//      a + b
+//    }
+//    def fun1(f:(Int, Int)=> Int, s:String) :String={
+//        val i = f(100,200)
+//       i+"#"+s
+//    }
+//
+//     val result = fun1((a:Int, b:Int)=>{
+//       a*b
+//     }, "scala");
+//    println(result)
+
+    //方法的返回是函数
+//    def fun(s:String) = {
+//      def fun1(s1:String, s2:String): String = {
+//      s1+ "##" + s2+ "$"+s
+//      }
+//      fun1 _
+//    }
+//
+//    println(fun("a")("b","c"))
+
+    //方法的返回和参数是函数
+//    def fun(f: (Int, Int) => Int) = {
+//      val i = f(1, 2)
+//
+//      def fun1(s1: String, s2: String): String = {
+//        s1 + "@" + s2 + "*" + i
+//      }
+//
+//      fun1 _
+//    }
+//
+//    val str = fun((a:Int,b:Int)=>{a+b})("a","b")
+//    println(str)
+
+    /**
+     * 颗粒化函数
+     */
+    def fun(a:Int, b:Int)(c:Int, d:Int)={
+      a+b+c+d
     }
+
+    println(fun(1,2)(3,4))
   }
   }
