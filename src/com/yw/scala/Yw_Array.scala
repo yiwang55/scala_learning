cf757882a05faf09ce51432e930ddcf1406b33eb
wangyi <wangyi@ghcchina.com.cn>
2020-08-20 17:22:03 +0800

    scala learning p6

---
+++ src/com/yw/scala/Yw_Array.scala
@@ -0,0 +1,8 @@
+package com.yw.scala
+
+object Yw_Array {
+  def main(args: Array[String]): Unit = {
+    val arr = Array[String]("a","b","c","d");
+    arr.foreach(println)
+  }
+}
---
+++ src/com/yw/scala/Yw_String.scala
@@ -0,0 +1,10 @@
+package com.yw.scala
+
+object Yw_String {
+  def main(args: Array[String]): Unit = {
+    val s = "yiwang"
+    val s1 = "YIWANG"
+
+
+  }
+}
